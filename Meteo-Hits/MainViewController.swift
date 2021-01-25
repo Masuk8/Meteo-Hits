432e3183c3493876d1126a04560741f9c014fc94
Martin Sikora <martin.sikora8@gmail.com>
2021-01-25 16:27:36 +0100

    Before connecting second view to controller.

---
+++ Meteo-Hits/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meteo-Hits
+//
+//  Created by Macbook on 25.01.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = "Meteo-Hits"
+  }
+
+
+}
+
---
+++ Meteo-Hits/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  Meteo-Hits
+//
+//  Created by Macbook on 25.01.2021.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Meteorite impact location"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
