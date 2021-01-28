56eadc5c3969de3bfe2e803ac95e63c291d42a1b
Martin Sikora <martin.sikora8@gmail.com>
2021-01-28 04:34:12 +0100

    Pins and camera center are done.

--- Meteo-Hits/ViewControllers/MapViewController.swift
+++ Meteo-Hits/ViewControllers/MapViewController.swift
@@ -10,12 +10,9 @@ import UIKit
 
 class MapViewController: UIViewController {
   
-  
-  
   var location: [Double] = []
-
-  @IBOutlet weak var mapView: MKMapView!
   
+  @IBOutlet weak var mapView: MKMapView!
   
   override func viewWillAppear(_ animated: Bool) {
     self.navigationController?.setNavigationBarHidden(false, animated: animated)
@@ -26,11 +23,26 @@ class MapViewController: UIViewController {
     self.navigationController?.setNavigationBarHidden(false, animated: animated)
   }
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      print(location)
-
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    placePin()
+    
+  }
+  func placePin() {
+    let lat = location[1]
+    let lon = location[0]
+    let annotation = MKPointAnnotation()
+    let centerCoordinate = CLLocationCoordinate2DMake(lat, lon)
+    annotation.coordinate = centerCoordinate
+    mapView.addAnnotation(annotation)
+    centerMapOnLocation()
+  }
+  
+  func centerMapOnLocation() {
+    let lat = location[1]
+    let lon = location[0]
+    let coordinate = CLLocationCoordinate2DMake(lat, lon)
+    mapView.centerCoordinate = coordinate
     }
- 
-
 }
+
